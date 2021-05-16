8ebe9a05bbe26b9556904dd55b315c62a8119032
Mark <mark@remarkablemark.org>
2021-05-15 22:55:04 -0400

    refactor(src): tidy auth.js exports

--- src/auth.js
+++ src/auth.js
@@ -2,8 +2,5 @@ const { auth } = require('@googleapis/analytics');
 const { CLIENT_EMAIL, PRIVATE_KEY } = require('./env');
 
 const scopes = 'https://www.googleapis.com/auth/analytics.readonly';
-const jwt = new auth.JWT(CLIENT_EMAIL, null, PRIVATE_KEY, scopes);
 
-module.exports = {
-  jwt,
-};
+exports.jwt = new auth.JWT(CLIENT_EMAIL, null, PRIVATE_KEY, scopes);
